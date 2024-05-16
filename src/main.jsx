c9b268423d12d8ab0777261ba1a72172d62e2a47
vidyaudayan <vidyaudayan89@gmail.com>
2024-05-16 13:52:43 +0530

    files changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react'
+{/*import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -42,4 +42,34 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <RouterProvider router={router} />
   </React.StrictMode>,
-)
+)*/}
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+import Root from './routes/root.jsx';
+
+import Home from './components/Pages/Home.jsx';
+import Destination from './components/Pages/Destination.jsx';
+import Activities from './components/Pages/Activities.jsx';
+import ErrorPage from './error-page.jsx';
+
+const AppRouter = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route path="home" element={<Home />} />
+        <Route path="destination" element={<Destination />} />
+        <Route path="activities" element={<Activities />} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
+);
